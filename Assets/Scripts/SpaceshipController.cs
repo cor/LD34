fbfa15b64b959ac6bcb3c377b06e37f56db37193
cor <cor@pruijs.nl>
2015-12-13 15:59:47 +0100

    Add Spaceship with movement

---
+++ Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipController : MonoBehaviour {
+
+	public float speed = 5;
+
+	public Transform leftThrust1;
+	public Transform leftThrust2;
+
+	public Transform rightThrust1;
+	public Transform rightThrust2;
+
+	public bool leftThrustActivated;
+	public bool rightThrustActivated;
+
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+
+		// initialize properties
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool leftDown = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+		bool rightDown = Input.GetKey(KeyCode.D) ||Input.GetKey(KeyCode.RightArrow);
+
+		leftThrustActivated = leftDown;
+		rightThrustActivated = rightDown;
+
+		Debug.Log("Left: " + leftDown);
+		Debug.Log("Right: " + rightDown);
+
+
+		ApplyThrusts();
+	}
+
+	void ApplyThrusts() {
+
+		Vector2 rightThrustVector = rightThrust2.position - rightThrust1.position;
+		Vector2 leftThrustVector = leftThrust2.position - leftThrust1.position;
+
+		if (leftThrustActivated) {
+			rb.AddForceAtPosition(leftThrustVector * speed * -1f, leftThrust1.position);
+		}
+
+		if (rightThrustActivated) {
+			rb.AddForceAtPosition(rightThrustVector * speed * -1f, rightThrust1.position);
+		}
+	}
+}
